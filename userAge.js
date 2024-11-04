ca47574b7b8dcd5951425df1235d87d02cd44069
Paulramirezz <ramirezjohnpaul747@gmail.com>
2024-11-04 19:41:36 +0800

    Create userAge.js

---
+++ userAge.js
@@ -0,0 +1,15 @@
+let age = "20";
+
+if (age < 5) {
+    console.log("You are a toddler.");
+} else if (age <= 9) {
+    console.log("You are a child.");
+} else if (age <= 11) {
+    console.log("You are a teenager.");
+} else if (age <= 20) {
+    console.log("You are a young adult.");
+} else if (age <= 60) {
+    console.log("You are middle-aged.");
+} else {
+    console.log("You are a senior.")
+}
